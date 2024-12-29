11cfe7b74f4a59c3d63c03d778e538beb24a84f8
Aleksey-Kireev <kireev_a_a@mail.ru>
2024-12-30 01:26:57 +0900

    2-ый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+      public static void main(String[] args) {
+// Задача 1
+    System.out.println("Задание № 1 ");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+// Задача 2
+
+
+
+    System.out.println("Задание № 2 ");
+
+// Задача 3
+    System.out.println("Задание № 3 ");
+
+
+
 
 
         }
